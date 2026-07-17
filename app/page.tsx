682feae6e913a8a112e72c3b81811aaf59f03d06
sujit gouda <sujit.gda997@gmail.com>
2026-07-17 13:19:53 +0530

    chore: removed startstory

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home(){
     <CareerTransformation />
     <CounsellorCta />
     <SuccessStories />
-    <StartYourStory />
+    {/* <StartYourStory /> */}
     <Faq />
    </>
   )
